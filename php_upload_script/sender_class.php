8730f940678c5784086d83d6372cf4fa97bc5065
werty1st <adams@werty1st.de>
2013-09-17 14:49:37 +0200

    init

---
+++ php_upload_script/sender_class.php
@@ -0,0 +1,94 @@
+<?php
+
+class sender{
+
+	protected $myxml;
+	private $sendungen;
+
+
+	function __construct ($mysender) {
+
+		global $url,$action;
+
+		$xml_in = new DOMDocument();
+		$xml_in->load($url."/".$mysender."?".$action);
+		
+
+		$this->myxml = new DOMDocument('1.0', 'UTF-8');
+		$xml_objectResponse 			= $this->myxml->createElement("objectResponse");
+
+			$miniEPG 	 				= $xml_in->getElementsByTagName("miniEPG")->item(0);
+			$xml_miniEPG 				= $this->myxml->createElement("tinyEPG");
+			$xml_miniEPG->setAttribute("miniEPGcontentId", $miniEPG->getAttribute("contentId"));
+
+				$xml_sendungen		 	= $this->myxml->createElement("sendungen");
+				$this->sendungen = $xml_sendungen;
+
+				$xml_miniEPG->appendChild( $xml_sendungen );
+			
+			$xml_objectResponse->appendChild( $xml_miniEPG );
+
+		$this->myxml->appendChild( $xml_objectResponse );
+
+
+		//sendungen durchparsen
+		$sendungen = $xml_in->getElementsByTagName('sendung');
+
+		//sendung verarbeiten
+		$imax = $sendungen->length;
+		$i1 = 1;
+		foreach ($sendungen as $sendung) {
+			$fortschritt = ($i1++)*100/$imax; $fortschritt = number_format($fortschritt, 2);
+		    $mysendung = new sendung($sendung);		    
+		    $mysendung->getComplete($fortschritt);
+		    $this->appendSendung($mysendung);
+		}		
+	}
+
+
+
+	public function toString($human = false) {
+
+		if( $human){
+		  	//Format XML to save indented tree rather than one line
+			$this->myxml->preserveWhiteSpace = false;
+			$this->myxml->formatOutput = true;		   
+		}
+
+		return $this->myxml->saveXML();
+	}
+
+	private function appendSendung($sendung) {
+		
+		$mySendungNode = $this->myxml->importNode($sendung->getNode(), true);
+		$this->sendungen->appendChild( $mySendungNode );
+	}
+
+	public function getSendungen(){
+
+		return $this->myxml->getElementsByTagName('sendung');
+	}
+
+	public function getSendungXML($sendung){
+
+		// $xmlstring = "";
+		// foreach ($sendung->childNodes as $node) {
+		// 	$xmlstring .= $this->myxml->saveXML($node);
+		// }
+
+		// return $xmlstring;
+
+		return $this->myxml->saveXML($sendung);
+	}
+}
+
+class sender_fromfile extends sender{
+
+	protected $myxml;
+
+	function __construct ($filename) {
+
+		$this->myxml = new DOMDocument();
+		$this->myxml->load($filename);
+	}	
+}
---
+++ php_upload_script/sendung_class.php
@@ -0,0 +1,72 @@
+<?php
+
+class sendung{
+
+	private $mysendung;
+	private $timestamp = 0;
+
+	function __construct ( $sendung ) {
+
+		$mysendung = new DOMDocument();
+		//$xml_sendung = $this->sendung->createElement("sendung");
+		
+		// Import the node, and all its children, to the document
+		$mySendungNode = $mysendung->importNode($sendung, true);
+
+		// And then append it to the "<root>" node
+		$mysendung->appendChild($mySendungNode);
+
+		//manipulate sendung
+		//$sendungsID = $sendung->getElementsByTagName('link')->item(0)->nodeValue;
+		//$content = "http://www.zdf.de/ZDF/zdfportal/xml/epg/20196106,014c0432-e900-31b2-9763-cd0a1f6c9cc3";	
+		//$preg_one = preg_match("/\,([^\,]*)$/", $sendungsID, $sendungsID);
+
+		$airtime 		= $sendung->getElementsByTagName('time')->item(0)->nodeValue;
+		$stationName 	= $sendungsID = $sendung->getElementsByTagName('station')->item(0);
+		$stationName 	= $stationName->getAttribute("name");
+
+		$prehash = $stationName."-".$airtime;
+		$hash 	 = md5($prehash);
+		$ID 	 = $mysendung->createElement("_id", $hash);
+		
+
+		//TODO reihenfolge stimmt nicht ID soll zuerst kommen
+		$firstChildA = $mysendung->getElementsByTagName('sendung')->item(0);
+		$firstChildB = $mysendung->getElementsByTagName('sendung')->item(0)->firstChild;
+		//var_dump($firstChild); exit;
+		$firstChildA->insertBefore($ID,$firstChildB);
+
+		// echo $mysendung->saveXML(); exit;
+
+		$this->mysendung = $mysendung;
+	}
+
+	public function getComplete($i){
+
+		//be communicative
+	    if (microtime(true) - $this->timestamp > 0.5) {
+	    	$this->timestamp = microtime(true);
+		    $out = $i."% Lade Details zur Sendung: ".$this->mysendung->getElementsByTagName('titel')->item(0)->nodeValue;
+		    
+		    console($out);
+		}
+		 
+
+		$xml_details = new DOMDocument();
+		$url = $this->mysendung->getElementsByTagName('link')->item(0)->nodeValue;
+		$xml_details->load($url);
+
+		$progdata = $xml_details->getElementsByTagName('programdata')->item(0);
+
+		// Import the node, and all its children, to the document
+		$progdata = $this->mysendung->importNode($progdata, true);
+
+		// And then append it to the "<root>" node
+		$this->mysendung->getElementsByTagName('sendung')->item(0)->appendChild($progdata);
+	}
+
+	public function getNode() {
+		return $this->mysendung->getElementsByTagName('sendung')->item(0);		
+	}
+
+}
