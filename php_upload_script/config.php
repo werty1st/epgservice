066a59cf2d1dde9301c920357c30879b10e938ff
werty1st <adams@werty1st.de>
2013-09-17 15:10:45 +0200

    init3

--- php_upload_script/config.php
+++ php_upload_script/config.php
@@ -14,8 +14,6 @@ $db_name = "epgservice";
 $db_host = "http://localhost:5984";
 
 
-
-kacke2
 //curl -X PUT -d "10" http://localhost:5984/epgservice/_revs_limit
 //curl -H "Content-Type: application/json" -X POST http://localhost:5984/epgservice/_compact
 
