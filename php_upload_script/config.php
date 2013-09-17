350508714646c54563ad603139fe2232f3707eec
werty1st <adams@werty1st.de>
2013-09-17 15:13:17 +0200

    dfg

--- php_upload_script/config.php
+++ php_upload_script/config.php
@@ -17,6 +17,8 @@ $db_host = "http://localhost:5984";
 //curl -X PUT -d "10" http://localhost:5984/epgservice/_revs_limit
 //curl -H "Content-Type: application/json" -X POST http://localhost:5984/epgservice/_compact
 
+test
+
 
 //XML Object Useragent
 $opts = array(
