c5c1900ed750a659bf7254d6132dcd57d05f2c8c
werty1st <adams@werty1st.de>
2013-10-09 17:25:53 +0200

    update

--- sofa_epgservice_db/lib/views/getall_view.js
+++ sofa_epgservice_db/lib/views/getall_view.js
@@ -1,9 +1,9 @@
 /*wird von php gebraucht*/
 exports.getall_view = {
 	map: function (doc) {
-        	if (doc.station !== undefined) {
-        		/*2013-08-28T05:00:00+02:00*/
-                emit(doc.station.name,{"_id":doc._id,"_rev":doc._rev});        		
-        	}
-    	}
+		if (doc.station !== undefined) {
+		/*2013-08-28T05:00:00+02:00*/
+			emit(doc.station.name,{"_id":doc._id,"_rev":doc._rev});        		
+		}
+	}
 };
--- sofa_epgservice_db/lib/views/getold_view.js
+++ sofa_epgservice_db/lib/views/getold_view.js
@@ -1,17 +1,17 @@
 /*wird von php gebraucht*/
 exports.getold_view = {
-	map: function (doc) {
-        	if (doc.station !== undefined) {
-        		/*2013-08-28T05:00:00+02:00*/
-        		var now = new Date();
-        		var old = new Date(doc.programdata.airtimeBegin);
+    map: function (doc) {
+        if (doc.station !== undefined) {
+        /*2013-08-28T05:00:00+02:00*/
+            var now = new Date();
+            var old = new Date(doc.programdata.airtimeBegin);
 
-        		var diff = ((now - old)/3600000); /*alter in stunden*/
-                diff = parseFloat(diff.toPrecision(6))
-                if (diff >= 30){        		
-        		 	emit(null,{"_id":doc._id,"_rev":doc._rev});
-                }
+            var diff = ((now - old)/3600000); /*alter in stunden*/
+            diff = parseFloat(diff.toPrecision(6))
+            if (diff >= 30) {
+                emit(null,{"_id":doc._id,"_rev":doc._rev});
+            }
 
-        	}
-    	}
+        }
+    }
 };
