5c4284729e214f1a737dd4567802cc74de149b50
werty1st <adams@werty1st.de>
2015-09-21 14:06:52 +0200

    phoenix html

--- sofa_epgservice_db/lib/lists/getliveDocCount.js
+++ sofa_epgservice_db/lib/lists/getliveDocCount.js
@@ -47,7 +47,7 @@ exports.getliveDocCount = function (head, req) {
 	}
 
 	if (debug) out.sendungen = 10;
-	if (out.sendungen < 220 || out.sendungen > 260) {
+	if (out.sendungen < 260 || out.sendungen > 300) {
 		out.statuscode = "Fehler: Es sind zu wenig oder zuviel Programmdaten online.";
 		//code = 503; 
 	}
