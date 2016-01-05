7ac1d80b0368534f792030c469d21df6518e622f
werty1st <adams@werty1st.de>
2016-01-05 13:52:16 +0100

    wechel auf jshint

--- olympia/main.js
+++ olympia/main.js
@@ -8,19 +8,19 @@ var current = "2014-02-05";
 //var current = moment();
 
 
-var options = { host: 'eventcms.zdf.de',
-                path: '/xml/olympia2014/epg/' };
+var options = { host: "eventcms.zdf.de",
+                path: "/xml/olympia2014/epg/" };
 
 
 
 //imports
-var https = require('https');
-var XmlStream = require('xml-stream');
-var moment = require('moment');
+var https = require("https");
+var XmlStream = require("xml-stream");
+//var moment = require("moment");
 
 
 //main
-var urls = require('./urlgen')({ startdate: startdate,
+var urls = require("./urlgen")({ startdate: startdate,
                                  enddate: enddate,
                                  current: current,
                                  path: options.path});
@@ -31,11 +31,11 @@ console.log(urls);
 function getXML(url,callback){
 
     console.log(url);
-    var request = https.get({host:options.host,path:url}).on('response', function(response) {
+    var request = https.get({host:options.host,path:url}).on("response", function(response) {
 
-        var xml = new XmlStream(response, 'utf8');
+        var xml = new XmlStream(response, "utf8");
 
-        xml.on('data', function(data) {
+        xml.on("data", function(data) {
             process.stdout.write(data);
         });
 
--- olympia/urlgen.js
+++ olympia/urlgen.js
@@ -1,62 +1,67 @@
 //urlgen
-var moment = require('moment');
+var moment = require("moment");
 
 
-
-module.exports = function (options){
+/**
+ * @param {object} options which have to include 
+ * startdate, enddate, current (current date) and a path
+ */
+module.exports = function (options){ 
 
 
 	//create date range urls
 	//days
-	var startd = moment(options.startdate);
-	var stopd = moment(options.enddate);
-	var today = moment(options.current);
-	var days = 0;
-	var skip = 0;
-	var urls = [];
+    var startd = moment(options.startdate);
+    var stopd = moment(options.enddate);
+    var today = moment(options.current);
+    var days = 0;
+    var skip = 0;
+    var urls = [];
+    var log = "";
 
 	//zufrüh
-	if (today.isBefore(startd)){
-	    console.log("zufrüh");
-	    //alle daten holen
-	    days = moment.duration(stopd.diff(startd)).asDays();
-	    skip = 0;
-	}
+    if (today.isBefore(startd)){
+        log = "zufrüh";
+        //alle daten holen
+        days = moment.duration(stopd.diff(startd)).asDays();
+        skip = 0;
+    }
 	//mittendrin
-	if ((today.isSameOrAfter(startd)) && (today.isSameOrBefore(stopd))) {
-	    console.log("mittendrin");
-	    //daten ab heute holen
-	    days = moment.duration(stopd.diff(today)).asDays();
-	    skip = moment.duration(today.diff(startd)).asDays();
-	}
+    if ((today.isSameOrAfter(startd)) && (today.isSameOrBefore(stopd))) {
+        log = "mittendrin";
+        //daten ab heute holen
+        days = moment.duration(stopd.diff(today)).asDays();
+        skip = moment.duration(today.diff(startd)).asDays();
+    }
 	//letzter tag
-	if (today.isSame(stopd)){
-	    console.log("schluss");
-	    //daten ab heute holen
-	    days = 1;
-	    skip = moment.duration(today.diff(startd)).asDays();
-	}
+    if (today.isSame(stopd)){
+        log = "schluss";
+        //daten ab heute holen
+        days = 1;
+        skip = moment.duration(today.diff(startd)).asDays();
+    }
 	//vorbei
-	if (today.isAfter(stopd)) {
-	    console.log("vorbei");
-	    //keiner daten holen
-	    days = 0;
-	    skip = 0;
-	}
+    if (today.isAfter(stopd)) {
+        log = "vorbei";
+        //keiner daten holen
+        days = 0;
+        skip = 0;
+    }
 
 	//add skip days
-	var date = startd.add(skip,'days');
+    var date = startd.add(skip,"days");
 
-	console.log(days);
-	console.log(skip);
+    for (var i=0;i<days;i++){
+        //url
+        var filename = date.format("YYYY-MM-DD");
+        date.add(1,"days");
+        var dpath = options.path + filename +".xml";
+        urls.push(dpath);
+    }
 
-	for (i=0;i<days;i++){
-	    //url
-	    var filename = date.format('YYYY-MM-DD');
-	    date.add(1,'days');
-	    dpath=options.path + filename +".xml";
-	    urls.push(dpath);
-	}
-
-	return urls;
-}
+    return { urls: urls,
+            days: days,
+            skip: skip,
+            log: log
+        };
+};
