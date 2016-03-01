b9ef966cc828fae410ba4f3af8920e04257fa94b
werty1st <adams@werty1st.de>
2016-03-01 18:20:06 +0100

    speichern

--- olympia/app/sender/SenderZDF.js
+++ olympia/app/sender/SenderZDF.js
@@ -1,11 +1,79 @@
 var moment = require("moment");
 var request = require('request').defaults({ encoding: null });
 var async = require("async");
-var https = require("https");
+var http = require("http");
 var flow = require("xml-flow");
+var xpathStream = require('xpath-stream');
+var _ = require('underscore');
 
 
 function SenderZDF(db){
+
+     /**
+     * addVideo creates a new Sendung Object and assigns data from xml object
+     * after that it collects the Sendungs Preview Image
+     * the done callback is passed from main.js@readXMLstream and gets passed to 
+     */
+    function addSendetermin (xmlElement, done){
+        
+               
+        //var sendung = new Sendung(db);
+        var sendung = {};
+        
+        sendung.externalId      = xmlElement.$attrs["externalId"];
+        sendung._id             = xmlElement.$attrs["externalId"];
+
+        sendung.vcmsid          = "vcmsidDummy";
+        sendung.vcmsChannelId   = "vcmsChannelIdDummy";
+        sendung.copy            = xmlElement.text;
+                
+        sendung.titel           = xmlElement.titel || "";
+        sendung.untertitel      = xmlElement.untertitel || "";
+        sendung.dachzeile       = xmlElement.dachzeile || "";
+        sendung.start           = xmlElement.beginnDatum;
+        sendung.end             = xmlElement.endeDatum;
+        sendung.channel         = "zdf";
+        
+        
+        if ( xmlElement.bildfamilie.ref ){
+        /**
+         * option1
+         *   bildfamilie: 
+         *      { ref: 'http://www.zdf.de/api/v2/content/p12:5989584',
+         *       type: 'VisualFamily' }
+         */            
+            sendung.imageUrl = "xmlElement.bildfamilie.ref";
+        } else if ( xmlElement.bildfamilie.zuschnitte ){
+        /** 
+         * option2
+         *  bildfamilie: 
+         *      { visibleFrom: { 'xsi:nil': 'true' },
+         *          visibleTo: { 'xsi:nil': 'true' },
+         *          zuschnitte: 
+         *          { '0': [Object],
+         *          '1': [Object]  
+         */      
+            sendung.imageUrl = "xmlElement.bildfamilie.zuschnitte";  
+        } else {
+            sendung.imageUrl = "URLdummy";            
+        }
+                
+        /*
+        sendung.title           = xmlElement.$markup.find(item => item.$name == "title").$markup[0];
+        sendung.copy            = xmlElement.$markup.find(item => item.$name == "copy").$markup[0];
+        sendung.start           = xmlElement.$markup.find(item => item.$name == "start").$markup[0];
+        sendung.end             = xmlElement.$markup.find(item => item.$name == "end").$markup[0];
+        sendung.channel         = "web" + xmlElement.$markup.find(item => item.$name == "channel").$markup[0];
+        sendung.image64         = "";
+        sendung.sportart = "?";
+        */
+
+         console.log(sendung.dachzeile," - ",sendung.imageUrl);
+         console.log();
+        
+         
+    }
+
 
 
     //xml stream reader
@@ -13,27 +81,38 @@ function SenderZDF(db){
      * @param {stream} stream from http get 
      * @param {function} callback to call after stream is closed 
      */
-    function readXML(stream, storeTagFn, doneXmlCb){
-        var xml = flow(stream, "utf8");
+    function readXML(stream, doneXmlCb){
+
+        var xml = flow(stream, {strict:true});
         
-        xml.on('tag:EPG', (x)=>{
-            console.log("nextPageLink",x);            
+        // getNavigation
+        stream
+            .pipe(xpathStream("EPG//navigation//Navigation//nextPageLink//Link//url//text()"))
+            .on('data',(x)=>{
+                var unescaped = _.unescape(x);
+                //console.log("currentPage:", currentPage++);
+                getEpgXml(unescaped);
+            });
+
+        // getSendungen
+        xml.on('tag:Sendetermin', (x)=>{
+            console.log("add sendetermin");
+            addSendetermin(x);            
         }); 
-        
-        
+
+        // Stream End
         xml.on("end", () =>{
             //doneXmlCb();        
-            console.log("done xml",stream);            
-                   
+            console.log("done xml");
             xml = null;
         });
     }
     
 
-    function getPage(url){
+    function getEpgXml(url){
         
-            console.log("getPage");
-            https.get(url, (res) => {
+            console.log("getEpgXml");
+            http.get(url, (res) => {
                 
                 if (res.statusCode != 200){
                     console.log(`Got response: ${res.statusCode} from ${url}`);
@@ -43,8 +122,7 @@ function SenderZDF(db){
                 }
             }).on('error', (e) => {
                 console.log(`Error in response: ${res}`);
-            });         
-                
+            });
     }
 
     /**
@@ -53,9 +131,9 @@ function SenderZDF(db){
      */
     this.update = function update(){
         var startd = moment().subtract(1, 'days').format("YYYY-MM-DD");
-        var stopd =  moment().add(30, 'days').format("YYYY-MM-DD");
+        var stopd =  moment().add(1, 'days').format("YYYY-MM-DD");
       
-        var url = `https://www.zdf.de/ZDF/zdfportal/api/v2/epg?station=zdf&startDate=${startd}&endDate=${stopd}`;
+        var url = `http://www.zdf.de/ZDF/zdfportal/api/v2/epg?station=zdf&startDate=${startd}&endDate=${stopd}`;
         
         console.log("url",url);
         
@@ -67,7 +145,7 @@ function SenderZDF(db){
          * <maxPage>14</maxPage>
          * <nextPageLink/> 
          */
-        getPage(url);
+        getEpgXml(url);
 
         
 
