4f3ce597493aa6efc5731e6629feec3027333347
root <root@wmaiz-v-sofa01>
2016-04-19 15:16:13 +0200

    monitor

--- monitoring/monitorApp.php
+++ monitoring/monitorApp.php
@@ -2,25 +2,49 @@
 
 
 $user_agent = 'PHP, ZDF Webmaster (dev)';
-#$url = "http://wmaiz-v-sofa01.dbc.zdf.de/epgservice/status";
+//$url = "http://wmaiz-v-sofa01.dbc.zdf.de/epgservice/status";
 $url = "http://sofa01.zdf.de/epgservice/status";
+//$url = "http://172.29.30.23:8787/";
 
 $ch = curl_init($url);
 curl_setopt($ch,  CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt ($ch, CURLOPT_USERAGENT, $user_agent); 
+curl_setopt ($ch, CURLOPT_TIMEOUT, 1); //1sec timeout
 
 
 /* Get the HTML or whatever is linked in $url. */
 $response = curl_exec($ch);
 
+/* Get curl error code*/
+$retcode = curl_errno($ch);
+// echo "retcode $retcode"; // 28 == timeout
+
 /* Check for 404 (file not found). */
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-if($httpCode != 200) {    
-	echo "sende Mail wegen Fehler $httpCode\n";
+
+
+if ($httpCode > 200) {
+	echo "Sende Mail wegen Fehler: $httpCode\n";
 	sende_mail($response);
 
+} else if ($retcode > 0){
+
+	if ($retcode==28){
+		$retcode="Timeout";
+		echo "restart couchdb\n";
+		$result=restart_couch();
+		echo $result."\n";
+	}
+        echo "Sende Mail wegen Fehler: $retcode\n";
+        sende_mail($response."\n".$result);
+
 } else {
- echo "Kein fehler\n";
+ echo "Kein Fehler.\n";
+}
+
+function restart_couch(){
+
+	return shell_exec("/sbin/restart couchdb");
 }
 
 function sende_mail($antwort) {
